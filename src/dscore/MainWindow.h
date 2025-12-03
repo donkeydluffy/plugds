6736b4a63de699884515fc7abdb34340245f7e44
Haifeng Zhang <haifeng.zhang@3d-scantech.com>
2025-12-03 17:36:37 +0800

    chore: delete some unused thing
    Signed-off-by: Haifeng Zhang <haifeng.zhang@3d-scantech.com>

--- src/dscore/MainWindow.h
+++ src/dscore/MainWindow.h
@@ -1,28 +1,4 @@
-/*
- * Copyright (C) 2020 Adrian Carpenter
- *
- * This file is part of Pingnoo (https://github.com/nedrysoft/pingnoo)
- *
- * An open-source cross-platform traceroute analyser.
- *
- * Created by Adrian Carpenter on 27/03/2020.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#ifndef PINGNOO_COMPONENTS_CORE_MAINWINDOW_H
-#define PINGNOO_COMPONENTS_CORE_MAINWINDOW_H
+#pragma once
 
 #include <QAbstractButton>
 #include <QAction>
@@ -99,5 +75,3 @@ class MainWindow : public QMainWindow {
   //! @endcond
 };
 }  // namespace sss::dscore
-
-#endif  // PINGNOO_COMPONENTS_CORE_MAINWINDOW_H
